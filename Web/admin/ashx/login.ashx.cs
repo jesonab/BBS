e999b7d776bb44eb3691c0a6ef28a01acb66f8f4
jeson Fu <645599892@qq.com>
2016-04-28 22:25:23 +0800

    2016 04 28

---
+++ Web/admin/ashx/login.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.admin.ashx
+{
+    /// <summary>
+    /// login 的摘要说明
+    /// </summary>
+    public class login : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
