f758467f11c0afb2ab167dce329dd6f597dc27c7
jeson Fu <645599892@qq.com>
2016-05-04 11:31:43 +0800

    20160504

---
+++ Web/admin/ashx/nav.ashx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.admin.ashx
+{
+    /// <summary>
+    /// nav 的摘要说明
+    /// </summary>
+    public class nav : IHttpHandler, System.Web.SessionState.IRequiresSessionState
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string action = context.Request.Form["Action"];
+            string json = "{}";
+            if (action == "Load")
+            {
+                if (context.Session["ID"] != null)
+                {
+                    json = "{\"info\":\"" + context.Session["Name"] + "\"}";
+                }
+
+            }
+
+            context.Response.Write(json);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+}
